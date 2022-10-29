4b2f907e9484cce020cabc88de4533c416680346
Alexandre <alexandre@debian11.localhost>
2022-10-30 00:07:41 +0200

    Add Tailwind Navbar

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar/Navbar';
+import Record from './components/Record/Record';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>OVH React Interface</p>
-      </header>
+      <Navbar />
+      <p>OVH React Interface</p>
+      <Record />
     </div>
   );
 }
---
+++ src/components/Record/Record.js
@@ -0,0 +1,4 @@
+//Load record based on zone specified in URL
+
+export default function Record() {
+}
